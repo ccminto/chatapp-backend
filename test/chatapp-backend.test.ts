6ccff3a8e314d43fc5144b83f44ac05f2d01872f
chris <chrizmmint@gmail.com>
2022-03-04 13:44:29 -0500

    first upload

---
+++ test/chatapp-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChatappBackend from '../lib/chatapp-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chatapp-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChatappBackend.ChatappBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
